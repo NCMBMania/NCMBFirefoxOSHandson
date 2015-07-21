d4151f065e29cff7f66c911762b195edbcac1fce
Administrator <admin@example.com>
2015-07-21 17:15:21 +0900

    Commit doc 3

--- sharephoto/app.js
+++ sharephoto/app.js
@@ -4,12 +4,33 @@ window.addEventListener("load", function() {
   var client_key = "be16d26ec36ce4dad0cff7f0c95aea8f122cb2a499832d8cd9b8acecc3fb2a43"; // クライアントキー
   NCMB.initialize(application_key, client_key);  // 初期化の実行  
   
-  var Hello = NCMB.Object.extend("Hello");
-  var hello = new Hello();
-  hello.set("message", "Hello World");
-  hello.save().then(function (obj) {
-    console.log("保存されました", obj);
-  }, function (err) {
-    console.log("保存失敗しました", err);
-  });
+  var GalleryController = {
+    init : function() {
+      console.log(GalleryController);
+      $('#image-file').change(function() {
+        GalleryController.upload();
+      });
+    },
+    
+    // 画像をアップロードする
+    upload : function() {
+      console.log("アップロード処理開始");
+      var fileInput = $("#image-file")[0];
+      if (fileInput.files.length > 0) {
+        var file = fileInput.files[0];            
+        if (!(/\.(png|jpg|jpeg|gif)$/i).test(file.name)) {
+          return true;
+        }
+        var ncmbFile = new NCMB.File(Date.now() + file.name, file);
+        ncmbFile.save().then(function() {
+          // アップロード成功
+          console.log("アップロードしました！");
+        }, function(error) {
+          // アップロード失敗
+          console.log("アップロード失敗しました", error);
+        });
+      }
+    }
+  };
+  GalleryController.init();
 });
