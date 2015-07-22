7b484b4a0542fd75f32a6f17110af64cab11710c
Administrator <admin@example.com>
2015-07-22 13:54:45 +0900

    Add 06

--- sharephoto/app.js
+++ sharephoto/app.js
@@ -72,14 +72,25 @@ window.addEventListener("load", function() {
   };
   
   var PhotoController = {
+    
+    // 初期化処理
     init : function() {
       this.filename = location.hash.replace(/^#/, "");
+      // コメント用オブジェクトを定義
+      this.Comment = NCMB.Object.extend("Comment"); // 追加
       // 戻るをタップした時の処理
       $('#back').on('click', function() {
         location.href = 'index.html';
       });
+      $("#comment").on("click", function (e) {
+        e.preventDefault();
+        PhotoController.addComment();
+      });
+
       // 写真を表示
       this.show();
+      // コメントを表示
+      this.showComments(); // 追加
     },
     
     // 写真の表示処理を行う
@@ -87,6 +98,54 @@ window.addEventListener("load", function() {
       var objFile = new NCMB.File(this.filename, null, null, null);
       objFile.fetchImgSource($('#img').get(0));
     },    
+    
+    // コメントの取得
+    showComments: function () {
+       // 検索用オブジェクト、NCMB.Queryのインスタンス作成
+       var query = new NCMB.Query(this.Comment);
+       
+       // ファイル名を検索条件に指定
+       query.equalTo("filename", this.filename);
+       
+       // 検索実行
+       query.find().then(function (comments) {
+           // 検索成功
+           // 既存の内容は一旦すべて破棄
+           $("#comments").empty();
+           PhotoController.renderComments(comments);
+       });        
+    },
+    
+    // コメント描画処理
+    renderComments: function(comments) {
+    	$(comments).each(function (i, comment) {
+        $("#comments").append("<div class='comment'>"+comment.get("message")+"</div>")
+       });
+    },
+    
+    // コメント追加処理
+    addComment: function () {
+      // 入力内容の取得
+      var message = $("#message").val();
+      
+      // コメントオブジェクトを作成（インスタンス化）
+      var comment = new this.Comment();
+      
+      // コメントおよびファイル名をセット
+      comment.set("filename", this.filename);
+      comment.set("message", message);
+      
+      // 保存処理の実行
+      comment.save().then(function (comment) {
+        // 成功時はまずコメント入力欄を消去
+        $("#message").val("");
+        
+        // コメントを追記で描画
+        PhotoController.renderComments([comment]);
+      }, function (e) {
+        console.debug(e); 
+      });
+    },
   };
 
   if (document.location.pathname == "/index.html") {
